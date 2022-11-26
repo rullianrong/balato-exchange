9eb10144d79b2032e3eaf8d889c0d5d6925c71ba
MisterScrooge <shawnhumbert13@gmail.com>
2022-11-26 09:18:04 +0800

    minor changes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -25,7 +25,8 @@ class Admin::UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        AdminConfirmationMailer.signed_up(@user).deliver_now
+        format.html { redirect_to admin_user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/mailers/admin_confirmation_mailer.rb
+++ app/mailers/admin_confirmation_mailer.rb
@@ -3,4 +3,9 @@ class AdminConfirmationMailer < ApplicationMailer
         @user = user
         mail(to: user.email, subject: 'Signup Confirmation Status')
     end
+
+    def signed_up(user)
+        @user = user
+        mail(to: user.email, subject: 'New Account Created')
+    end
 end
